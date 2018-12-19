5afefd630c9a2649a4b5fcaa334e08672c49f666
wanghaobing <haobingwang@outlook.com>
2018-12-19 19:13:51 +0800

    调整嵌套路由并增加 403 404 500 路由

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Main from './views/layout/Main.vue'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
@@ -10,21 +11,37 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      redirect: { name: 'home' },
+      component: Main,
+      children: [
+        {
+          path: 'home',
+          name: 'home',
+          component: Home
+        },
+        {
+          path: 'about',
+          name: 'about',
+          component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+        },
+        {
+          path: 'hello',
+          name: 'hello',
+          component: () => import(/* webpackChunkName: "hello" */ './views/Hello.vue')
+        }
+      ]
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/403',
+      component: () => import(/* webpackChunkName: "403" */ './views/403.vue')
     },
     {
-      path: '/hello',
-      name: 'hello',
-      component: () => import(/* webpackChunkName: "about" */ './views/Hello.vue')
+      path: '/500',
+      component: () => import(/* webpackChunkName: "500" */ './views/500.vue')
+    },
+    {
+      path: '*',
+      component: () => import(/* webpackChunkName: "404" */ './views/404.vue')
     }
   ]
 })
