7c281fd2c7caceef6239c87257772de18d24f94f
wanghaobing <haobingwang@outlook.com>
2018-12-19 17:23:11 +0800

    添加 Bootstrap 演示页面

---
+++ src/views/Hello.vue
@@ -0,0 +1,6 @@
+<template>
+  <b-jumbotron header="Bootstrap Vue" lead="Bootstrap 4 Components for Vue.js 2" >
+    <p>For more information visit website</p>
+    <b-btn variant="primary" href="#">More Info</b-btn>
+  </b-jumbotron>
+</template>
